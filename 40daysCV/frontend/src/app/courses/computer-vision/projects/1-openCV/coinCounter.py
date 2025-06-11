3fbf306dfc70582e37c89b7080e99d757a86a714
Swapnil Gupta <swapnilgupta.xi@gmail.com>
2025-06-11 10:59:38 +0530

    coin-counter

---
+++ 40daysCV/frontend/src/app/courses/computer-vision/projects/1-openCV/coinCounter.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy
+
+#converting to grayscale 
+#applying gaussian blur
+#applying thresholding for des
+#applying more blur
+
+imgPath=""
+image= cv2.imread("https://github.com/swapnilxi/A-computer-vision/blob/open-cv-projects/40daysCV/frontend/src/app/courses/computer-vision/projects/ImagesData/coinImage.jpg")
